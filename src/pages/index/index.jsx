85a116dedddcc2a3de90d5363342b485c27f5097
Lin HU <lin.hu@it-consultis.com>
2020-08-19 11:45:39 +0800

    update demo

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -4,7 +4,7 @@ import './index.css'
 
 export default class Index extends Component {
   render () {
-    const content = '<p style = "font-size=35px;color=grey"> With its unique classic Scandinavian feel condensed into a design, the Nerd Chair won the Muuto Talent Award in 2011.</p>';
+    const content = '<p style = "font-size=35px;color=grey"><img src="https://i.picsum.photos/id/661/300/500.jpg?hmac=moOSuJ4Pwk8Ko5-r-u_50KQMjG8DPDnevqhxK5AvSfc" />  With its unique classic Scandinavian feel condensed into a design, the Nerd Chair won the Muuto Talent Award in 2011.</p>';
     return (
       <View className='index'>
         <View dangerouslySetInnerHTML={{ __html: content }}></View>
